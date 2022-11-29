bf057e3e310ae250e64d600f9aefbdd0c256108a
Enilson Neto01~ <enilsonneto8@gmail.com>
2022-11-29 11:25:26 -0300

    Projeto tabela de numeros em Java

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Variável , Quadrado , Cubo");
+        System.out.println("");
+
+
+        for (int x = 0; x <= 10; x++) {
+            System.out.println("  " +  x  + "    " + (x*x) + "    " + (x*x*x));
+        }
+    }
+}
